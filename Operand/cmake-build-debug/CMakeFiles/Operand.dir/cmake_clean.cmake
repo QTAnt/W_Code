ac62b73d86c59f4ef890e120b44b008e9fd5bd21
QTAnt <zhangcong129@126.com>
2022-03-20 23:20:40 +0800

    Operand

---
+++ Operand/cmake-build-debug/CMakeFiles/Operand.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Operand.dir/main.c.o"
+  "Operand"
+  "Operand.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Operand.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
